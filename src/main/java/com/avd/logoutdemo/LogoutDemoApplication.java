1c1b3e208ace6dee2e51c93c55b5735d7144b631
adamdewingMedtronic <adam.v.dewing@medtronic.com>
2020-08-27 10:08:11 -0500

    Initial commit

---
+++ src/main/java/com/avd/logoutdemo/LogoutDemoApplication.java
@@ -0,0 +1,13 @@
+package com.avd.logoutdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogoutDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogoutDemoApplication.class, args);
+    }
+
+}
